a6e8042e62052e8df81cac8bac284fd828f7ae0f
Mehedi809 <119084888+Mehedi809@users.noreply.github.com>
2022-12-20 17:41:19 +0600

    Update hey.py

--- hey.py
+++ hey.py
@@ -1,21 +1,28 @@
+#my project is image download in online site
 import requests
 
-First_inp = int(input("Enter a total image: "))   #this is total image user input
+def Dnload_pro(a):
+    First_inp = int(input("Enter a total image: "))   #Total image user input
+    url_dict = {}  # This is empty dictionary
 
-# This is empty dictionary
-url_dict = {}
+    # This loop is input 
+    for i in range (1, First_inp+1):
+        img_name = input("Please image name here: ")
+        img_url = input("Please 'copy image address' here: ")
+        url_dict[img_name]=img_url
 
-# This loop is input 
-for i in range (1, First_inp+1):
-    img_name = input("Please image name here: ")
-    img_url = input("Please 'copy image address' here: ")
-    url_dict[img_name]=img_url
+    # This loop is dict key and value different variable
+    for x, y in url_dict.items():
+        r = requests.get(y) #This url var is y  
 
-# This loop is dict key and value different variable
-for x, y in url_dict.items():
-    r = requests.get(y) #This url var is y  
+        with open(x, "wb") as f: #This is print the name and url hit , then output at download image
+            f.write(r.content)
 
-    with open(x, "wb") as f: #This is print the name and url hit , then output at download image
-        f.write(r.content)
-        
-# This my project, please check this program and where is problem inform me
+func_inp = input("Is the code really useful to you? If yes then write 'Yes' and if not then write 'No'.: ")
+if func_inp=="Yes":
+    print(Dnload_pro(func_inp))   #call function
+else:
+    print("Then this code will not be of any use to you, thanks.")
+
+            
+    # This my project, please check this program code and where is problem inform me
